d4b975615df6a156686da6d4a4414e5b8bae749f
VobilishettyPrashanth <vobilishetty.p@northeastern.edu>
2023-03-28 21:33:45 -0400

    A-f-webap-changes

--- src/main/java/edu/neu/coe/csye6225/webapp/controller/TestController.java
+++ src/main/java/edu/neu/coe/csye6225/webapp/controller/TestController.java
@@ -1,5 +1,8 @@
 package edu.neu.coe.csye6225.webapp.controller;
-
+import com.timgroup.statsd.StatsDClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,12 +11,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 import edu.neu.coe.csye6225.webapp.model.UserDto;
 
+
+
 @RestController
 @RequestMapping("/healthz")
-public class TestController {
 
+
+
+public class TestController {
+	@Autowired
+	private StatsDClient statsDClient;
+	private static final Logger logger = LoggerFactory.getLogger(TestController.class);
 	@GetMapping()
 	public ResponseEntity<?> getHealth() {
+		logger.info("Check of Healthz ");
+		statsDClient.incrementCounter("endpoint.Healthz.http.get");
 		return new ResponseEntity<UserDto>( HttpStatus.OK);
 	}
 }
